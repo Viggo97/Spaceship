cc1c1bb71f9eec09b44b51988fcde83084492596
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2021-03-24 13:43:05 +0100

    Add enemies generating

---
+++ js/Enemy.js
@@ -0,0 +1,39 @@
+export class Enemy {
+    constructor(container, intervalTime, enemyClass, lives = 1) {
+        this.container = container;
+        this.enemyClass = enemyClass;
+        this.element = document.createElement('div');
+        this.interval = null;
+        this.intervalTime = intervalTime;
+        this.lives = lives;
+    }
+
+    init() {
+        this.#setEnemy();
+        this.#updatePosition();
+    }
+
+    #setEnemy() {
+        this.element.classList.add(this.enemyClass);
+        this.container.appendChild(this.element);
+        this.element.style.top = '0px';
+        this.element.style.left = `${this.#randomPosition()}px`;
+    }
+
+    #randomPosition() {
+        return Math.floor(Math.random() * (window.innerWidth - this.element.offsetWidth));
+    }
+
+    #updatePosition() {
+        this.interval = setInterval(() => this.#setNewPosition(), this.intervalTime);
+    }
+
+    #setNewPosition() {
+        this.element.style.top = `${this.element.offsetTop + 1}px`;
+    }
+
+    remove() {
+        clearInterval(this.interval);
+        this.element.remove();
+    }
+}
--- js/Game.js
+++ js/Game.js
@@ -1,13 +1,19 @@
 import { Spaceship } from './Spaceship.js';
+import { Enemy } from './Enemy.js';
 
 class Game {
     #htmlElements = {
         container: document.querySelector('[data-container]'),
         spaceship: document.querySelector('[data-spaceship]'),
+
     };
 
     #ship = new Spaceship(this.#htmlElements.spaceship, this.#htmlElements.container);
+    #enemies = [];
     #checkPositionInterval = null;
+    #createEnemyInterval = null;
+    #enemiesInterval = null;
+
     init() {
         this.#ship.init();
         this.#newGame();
@@ -15,6 +21,8 @@ class Game {
 
     #newGame() {
         this.#checkPositionInterval = setInterval(() => this.#checkPosition(), 1);
+        this.#createEnemyInterval = setInterval(() => this.#randomNewEnemy(), 2000);
+        this.#enemiesInterval = 30;
     }
 
     #checkPosition() {
@@ -31,6 +39,33 @@ class Game {
                 missileArr.splice(missileIndex, 1);
             }
         });
+
+        this.#enemies.forEach((enemy, enemyIndex, enemiesArr) => {
+            const enemyPosition = {
+                top: enemy.element.offsetTop,
+                right: enemy.element.offsetLeft + enemy.element.offsetWidth,
+                bottom: enemy.element.offsetTop + enemy.element.offsetHeight,
+                left: enemy.element.offsetLeft,
+            }
+
+            if (enemyPosition.top > window.innerHeight) {
+                enemy.remove();
+                enemiesArr.splice(enemyIndex, 1);
+            }
+        });
+    }
+
+    #createNewEnemy(...params) {
+        const enemy = new Enemy(...params);
+        enemy.init();
+        this.#enemies.push(enemy);
+    }
+
+    #randomNewEnemy() {
+        const randomNumber = Math.floor(Math.random() * 5) + 1;
+        randomNumber % 5 
+        ? this.#createNewEnemy(this.#htmlElements.container, this.#enemiesInterval, 'enemy') 
+        : this.#createNewEnemy(this.#htmlElements.container, this.#enemiesInterval *  2, 'enemy--big', 3);
     }
 }
 
